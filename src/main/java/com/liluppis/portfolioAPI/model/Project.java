34d48e8a8a670cdb591a14616c8acd4de63c6abc
liluppis <lisa.upp@hotmail.com>
2025-10-09 08:20:28 +0200

    add: attributes + constructors to model

--- src/main/java/com/liluppis/portfolioAPI/model/Project.java
+++ src/main/java/com/liluppis/portfolioAPI/model/Project.java
@@ -1,7 +1,38 @@
 package com.liluppis.portfolioAPI.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document("projects")
+import java.util.List;
+
+@Getter
+@Setter
+@Document(collection = "projects")
 public class Project {
+
+    @Id
+    private String id;
+    @Field(name = "projectName")
+    private String name;
+    @Field(name = "description")
+    private String desc;
+    @Field(name = "link")
+    private String link;
+    @Field(name = "tags")
+    private List<String> tags;
+    @Field(name = "iconKey")
+    private String iconKey;
+
+    public Project() {}
+
+    public Project(String name, String desc, String link, List<String> tags, String iconKey) {
+        this.name = name;
+        this.desc = desc;
+        this.link = link;
+        this.tags = tags;
+    }
+
 }
