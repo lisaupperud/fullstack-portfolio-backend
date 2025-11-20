e6df396d7d5d48af0b6a0e3b9132e67b5486fe40
liluppis <lisa.upp@hotmail.com>
2025-11-20 15:54:45 +0100

    refactor: cors configuration to only allow frontend

--- src/main/java/com/liluppis/portfolioAPI/project/config/WebConfig.java
+++ src/main/java/com/liluppis/portfolioAPI/project/config/WebConfig.java
@@ -2,15 +2,17 @@ package com.liluppis.portfolioAPI.project.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/**") // apply config to ALL endpoints in backend
+                .allowedOrigins("http://localhost:5173") // only allow CORS requests from this origin
+                .allowedMethods("GET") // defines HTTP methods frontend is allowed to use when making requests
+                .allowedHeaders("*") // allows for any Headers the frontend sends
+                .allowCredentials(true); //tells browser it's ok to include credentials
     }
 }
