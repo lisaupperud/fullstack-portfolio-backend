d35874fa372c8f4f312b298a7613fd30b01e65db
liluppis <lisa.upp@hotmail.com>
2026-01-23 16:24:31 +0100

    Refactor health endpoint to return json instead of String

--- src/main/java/com/liluppis/portfolioAPI/project/controller/HealthCheckController.java
+++ src/main/java/com/liluppis/portfolioAPI/project/controller/HealthCheckController.java
@@ -1,19 +1,28 @@
 package com.liluppis.portfolioAPI.project.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-// Todo - make endpoint return 503 if status is not OK
-// return response body
 
 @RestController
 @RequestMapping("/api/v1")
 public class HealthCheckController {
 
     @GetMapping("/health")
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
+    public ResponseEntity<HealthCheckResponse> healthCheck() {
+        try {
+            HealthCheckResponse healthOk = new HealthCheckResponse(200, "OK");
+            return ResponseEntity.ok(healthOk);
+        } catch (Exception ex) {
+            HealthCheckResponse healthBad = new HealthCheckResponse(503, "Service Unavailable");
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(healthBad);
+        }
     }
+
+    public record HealthCheckResponse(
+            int status,
+            String message
+    ){}
 }
