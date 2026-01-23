0486140a67ae53c4b1875693e92fa524d99bd989
liluppis <lisa.upp@hotmail.com>
2026-01-23 16:11:56 +0100

    Add RequestMapping for endpoint path

--- src/main/java/com/liluppis/portfolioAPI/project/controller/HealthCheckController.java
+++ src/main/java/com/liluppis/portfolioAPI/project/controller/HealthCheckController.java
@@ -2,12 +2,14 @@ package com.liluppis.portfolioAPI.project.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // Todo - make endpoint return 503 if status is not OK
 // return response body
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 public class HealthCheckController {
 
     @GetMapping("/health")
